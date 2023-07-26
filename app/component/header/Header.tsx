3338459de1052c4de6d1abd7dfb19bcc04f6757e
PracticeKJY <poiijtl1004@naver.com>
2023-07-27 00:55:17 +0900

    ✨ feat. 헤더부분 완성

--- app/component/header/Header.tsx
+++ app/component/header/Header.tsx
@@ -1,6 +1,6 @@
-"use client"
+"use client";
 
-import Link from "next/link"
+import Link from "next/link";
 
 const Header = () => {
   return (
@@ -11,9 +11,10 @@ const Header = () => {
       w-[300px] sm:w-[500px]"
       >
         <h1 className="text-3xl font-extrabold">나의 성장 이야기</h1>
-        <div className="mt-4">
-          <span className="">글쓴이: 김종엽</span>
-          <time className=" pl-4">2023.07.24</time>
+        <div className="mt-4 text-gray-400 text-sm">
+          <span>글쓴이: 김종엽</span>
+          <span className=" pl-4">입력: </span>
+          <time>2023.07.24</time>
         </div>
         <p className="mt-10 leading-[30px]">
           이 페이지는 BBC 비주얼저널리즘 팀에서 제작한{" "}
@@ -27,12 +28,17 @@ const Header = () => {
           </Link>{" "}
           페이지를 비슷하게 구현해 본 개발 예제입니다.
           <br />
-          아래의 내용은 프론트엔드를 처음 접한 이후 부터 지금까지의 저의
-          이야기를 짧게 담아 봤습니다.
+          포트폴리오를 만들면서 Intersection Observer을 배웠는데, 이 페이지를
+          보자마자 배운걸 써먹어봐야겠다! 라고 생각했어요.
+          <br />
+          이미지는 직접 그린 것 & 유튜브 1분코딩님의 이미지로 구성하였고,
+          <br />
+          내용은 프론트엔드를 처음 접한 이후 부터 지금까지의 저의 이야기를 짧게
+          담아 봤습니다.
         </p>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
