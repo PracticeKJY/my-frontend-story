4bbc618d4e6e96ebfe637ebef6eacf9e236b6417
PracticeKJY <poiijtl1004@naver.com>
2023-07-27 12:04:24 +0900

    🎨 modify. Header.tsx 수정
    - width속성을 수정하였습니다.

--- app/component/header/Header.tsx
+++ app/component/header/Header.tsx
@@ -1,6 +1,6 @@
-"use client";
+"use client"
 
-import Link from "next/link";
+import Link from "next/link"
 
 const Header = () => {
   return (
@@ -8,7 +8,7 @@ const Header = () => {
       <div
         className="mt-20 lg:mt-40 mx-10 px-5 
       flex flex-col 
-      w-[300px] sm:w-[500px]"
+      w-[320px] sm:w-[520px]"
       >
         <h1 className="text-3xl font-extrabold">나의 성장 이야기</h1>
         <div className="mt-4 text-gray-400 text-sm">
@@ -31,6 +31,7 @@ const Header = () => {
           포트폴리오를 만들면서 Intersection Observer을 배웠는데, 이 페이지를
           보자마자 배운걸 써먹어봐야겠다! 라고 생각했어요.
           <br />
+          <br />
           이미지는 직접 그린 것 & 유튜브 1분코딩님의 이미지로 구성하였고,
           <br />
           내용은 프론트엔드를 처음 접한 이후 부터 지금까지의 저의 이야기를 짧게
@@ -38,7 +39,7 @@ const Header = () => {
         </p>
       </div>
     </header>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
