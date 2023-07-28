abf01d0dca7f5bf8cc3ce71d68d8aa740a55c4d4
PracticeKJY <poiijtl1004@naver.com>
2023-07-28 17:34:51 +0900

    🎨 modify. body,footer 텍스트 수정

--- app/component/footer/Footer.tsx
+++ app/component/footer/Footer.tsx
@@ -5,39 +5,6 @@ const Footer = () => {
     <>
       <footer className="relative flex flex-col items-center">
         <div
-          className=" mx-10 px-5 
-      flex flex-col 
-      w-[300px] sm:w-[500px]"
-        >
-          <h2 className="text-3xl font-extrabold">요약하자면요..</h2>
-          <p className="mt-4 leading-[30px]">
-            1. 컴공과를 전공하였지만, 편입 + 군입대 + 코로나 3연타 맞음.
-            <br />
-            2. 전문성이 없어 악착같이 제로부터 도전함.
-            <br />
-            3. 어느정도 사람 구실은 할 수 있을 정도로 성장함.
-            <br />
-            4. 언능 취업하고싶다. 아자아자!!
-          </p>
-        </div>
-        <div
-          className="mt-10 px-5 
-      flex flex-col 
-      w-[300px] sm:w-[500px]"
-        >
-          <h2 className="text-3xl font-extrabold">그동안 생긴 지병들</h2>
-          <h3 className="line-through">(나이가 30 넘어서 생긴거 아님)</h3>
-          <p className="mt-4 leading-[30px]">
-            1. 일자목(5,6번 척추손상)
-            <br />
-            2. 허리측만증 (꾸불꾸불 내 허리)
-            <br />
-            3. 대싱포진 (수면부족,영양실조)
-            <br />
-            4. 만성피로 (자도자도 피곤해요)
-          </p>
-        </div>
-        <div
           className="m-10 px-5 
       flex flex-col 
       w-[300px] sm:w-[500px]"
@@ -51,6 +18,34 @@ const Footer = () => {
             3. Intersection Observer(JS)
             <br />
             4. TypeScript
+          </p>
+        </div>
+        <div
+          className="m-10 px-5 
+      flex flex-col 
+      w-[300px] sm:w-[500px]"
+        >
+          <h2 className="text-3xl font-extrabold">만들면서 고민했던 것</h2>
+          <p className="mt-4 leading-[30px]">
+            1. 설계부분의 이미지
+            <br />
+            {"=> 직접 그림.."}
+            <br />
+            2. 너무나 길어진 코드
+            <br />
+            {"=> custom hook의 사용 + 컴포넌트 분리로 관리"}
+          </p>
+        </div>
+        <div
+          className="m-10 px-5 
+      flex flex-col 
+      w-[300px] sm:w-[500px]"
+        >
+          <h2 className="text-3xl font-extrabold">도전해보고 싶은 것</h2>
+          <p className="mt-4 leading-[30px]">
+            1. 만든 프로젝트에 lazy Loading 적용
+            <br />
+            2. interective web
           </p>
         </div>
       </footer>
